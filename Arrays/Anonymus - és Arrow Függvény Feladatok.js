b8ce094fc5315f3c4061497cea0a3147c2dfe633
Wittner Attila <b1witatt@vasvari.org>
2025-09-04 15:34:20 +0200

    functions done

--- Arrays/Anonymus - és Arrow Függvény Feladatok.js
+++ Arrays/Anonymus - és Arrow Függvény Feladatok.js
@@ -1,14 +1,4 @@
 /*
-Feladat:
-Adottak az alábbi tömbök:
-
-
-Hozd létre az alábbi függvényeket:
-sortByLength() - String tömböt a szavak hossza szerint rendezi és a rendezett tömböz adja vissza
-sortByLengthAsc() - String tömböt a szavak hossza szerint rendezi ABC sorrendbe és a rendezett tömböz adja vissza
-sortFrom15() - számokat rendez a 15-től való távolság alapján és a rendezett tömböt adja vissza
-addAsterisk() - String tömb mindegy elemének az elejére és végére egy csillagot tesz és visszaadja a módosított tömböt
-between5And15() - számokat tartalmazó tömb 5 és 15 közötti elemeit adja vissza egy tömbben
 isAllOdd() - számokat tartalmazó tömb minden eleme páratlan-e. Visszatérési érték true vagy false
 hasEven() - számokat tartalmazó tömb tartalmaz-e páros elemet. Visszatérési érték true vagy false
 sigma() - számokat tartalmazó tömb elemeit összeszorozza és a szorzatot adja vissza
@@ -36,11 +26,20 @@ const numbers = [2, 13, 3, 7, 17, 5, 11, 19, 9];
 const names = ['Eva', 'Adel', 'Cedric', 'Dior', 'Frank', 'Bob'];
 const fruits = ['pineapple', 'kiwi', 'banana', 'pear', 'cherry'];
 
-const sortByLength = () => {};
-const sortByLengthAsc = () => {};
-const sortFrom15 = () => {};
-const addAsterisk = () => {};
-const between5And15 = () => {};
-const isAllOdd = () => {};
-const hasEven = () => {};
-const sigma = () => {};
+const sortByLength = () => [...names].sort((a, b) => a.length - b.length);
+const sortByLengthAsc = () => [...names].sort((a, b) => a.localeCompare(b));
+const sortFrom15 = () => [...numbers].sort((a, b) => Math.abs(15 - a) - Math.abs(15 - b));
+const addAsterisk = () => [...fruits].map((fruit) => `*${fruit}*`);
+const between5And15 = () => [...numbers].filter((number) => number > 5 && number < 15);
+const isAllOdd = () => [...numbers].filter((number) => number % 2 === 1).length === 0;
+const hasEven = () => [...numbers].filter((number) => number % 2 === 0).length > 0;
+const sigma = () => [...numbers].reduce((acc, curr) => acc * curr, 1);
+
+console.log(sortByLength());
+console.log(sortByLengthAsc());
+console.log(sortFrom15());
+console.log(addAsterisk());
+console.log(between5And15());
+console.log(isAllOdd());
+console.log(hasEven());
+console.log(sigma());
