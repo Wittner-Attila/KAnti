5dc2c8ecd904c93a3895ba51e448c5631a247f48
Wittner Attila <b1witatt@vasvari.org>
2025-09-04 15:16:05 +0200

    prettier

--- Arrays/Array Feladatok.js
+++ Arrays/Array Feladatok.js
@@ -11,71 +11,73 @@ Mindegyik feladatot függvénnyel old meg!
 */
 
 const getOtosLotteryNumbers = () => {
-    const lotteryNumbers = [];
-    while (lotteryNumbers.length < 5) {
-        const randomNumber = Math.floor(Math.random() * 9) + 1;
-        if (!lotteryNumbers.includes(randomNumber)) {
-            lotteryNumbers.push(randomNumber);
-        }
+  const lotteryNumbers = [];
+  while (lotteryNumbers.length < 5) {
+    const randomNumber = Math.floor(Math.random() * 9) + 1;
+    if (!lotteryNumbers.includes(randomNumber)) {
+      lotteryNumbers.push(randomNumber);
     }
-    return lotteryNumbers;
-}
+  }
+  return lotteryNumbers;
+};
 
 let lotteryNumbers = getOtosLotteryNumbers();
 
 console.log(lotteryNumbers);
 
 const getSortedLotteryNumbers = (numbers) => {
-    return numbers.slice().sort((a, b) => a - b);
-}
+  return numbers.slice().sort((a, b) => a - b);
+};
 
 console.log(getSortedLotteryNumbers(lotteryNumbers));
 
 const getNumberOfHits = (lotteryNumbers, userTips) => {
-    let hits = 0;
-    userTips.forEach(userTip => {
-        if (lotteryNumbers.includes(userTip)) {
-            hits++;
-        }
-    });
-    return hits;
-}
+  let hits = 0;
+  userTips.forEach((userTip) => {
+    if (lotteryNumbers.includes(userTip)) {
+      hits++;
+    }
+  });
+  return hits;
+};
 
 console.log("\n3. Feladat:");
 console.log(getNumberOfHits(lotteryNumbers, [5, 12, 23, 34, 45]));
 
 const getMonthlyLotteryArrayNumbers = () => {
-    const monthlyStatistics = [];
-    for (let i = 0; i < 4; i++) {
-        monthlyStatistics.push(getOtosLotteryNumbers());
-    }
-    return monthlyStatistics;
-}
+  const monthlyStatistics = [];
+  for (let i = 0; i < 4; i++) {
+    monthlyStatistics.push(getOtosLotteryNumbers());
+  }
+  return monthlyStatistics;
+};
 
 let monthlyNumbers = getMonthlyLotteryArrayNumbers();
 
 console.log("\n4. Feladat:");
 console.log(monthlyNumbers);
 
-const getMonthlyLotteryArrayNumbersUnique = (monthlyNumbers) => [...new Set(monthlyNumbers)];
+const getMonthlyLotteryArrayNumbersUnique = (monthlyNumbers) => [
+  ...new Set(monthlyNumbers),
+];
 
 console.log("\n5. Feladat:");
 console.log(getMonthlyLotteryArrayNumbersUnique(monthlyNumbers));
 
 const monthlyStatistics = (monthlyNumbers) => {
-    let stats = [];
-    monthlyNumbers.forEach(week => {
-        week.forEach(number => {
-            let stat = stats.find(stat => stat[0] === number);
-            if (stat) {
-                stat[1]++;
-            } else {
-                stats.push([number, 1]);
-            }
-        })
-    })
-    return stats
-}
+  let stats = [];
+  monthlyNumbers.forEach((week) => {
+    week.forEach((number) => {
+      let stat = stats.find((stat) => stat[0] === number);
+      if (stat) {
+        stat[1]++;
+      } else {
+        stats.push([number, 1]);
+      }
+    });
+  });
+  return stats;
+};
 
 console.log("\n6. Feladat:");
 console.log(monthlyStatistics(monthlyNumbers));
